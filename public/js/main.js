560c81259abc47c1b063bcecbe4aa7ba6ea6f2b4
Brian Lam <brianlam@stanford.edu>
2014-08-18 00:29:25 -0700

    reached mvp!

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,29 @@
-$(document).ready(function() {
-    console.log("document ready");
-    $.ajax({
-        url: window.location.origin + "/data",
-        method: "GET",
-        data: {"metrics" : ["Pushups", "Photos Uploaded", "Pages Read", "Running Distance"]},
-        success: function(data, status) {
-            debugger;
-            $metrics = $("#metrics");
-            for (var metric in data) {
-                $metrics.append("<li>" + metric + ": " + Number((data[metric]).toFixed(2)));
-            }
+$(function() {
+    function createMetricItem(name, value) {
+        var $item = $(document.createElement('li'));
+        $item.addClass("metric-item");
+        $item.append('<p class="metric-value">' + Number(Number(value).toFixed(2)) + '</p>');
+        $item.append('<p class="metric-name">' + name + '</p>');
+        return $item;
+    }
+
+    function displayMetrics(data, status) {
+        debugger;
+        $metrics = $("#metric-list-1");
+        for (var metric in data) {
+            $metrics.append(createMetricItem(metric, data[metric]));
         }
+    }
+
+    $(document).ready(function() {
+        var daily = ["Pushups", "Photos Uploaded", "Pages Read"];
+        var ytd = ["Running Distance", "Transport Distance"];
+
+        $.ajax({
+            url: window.location.origin + "/data",
+        method: "GET",
+        data: {"daily" : daily, "ytd":ytd},
+        success: displayMetrics
+        });
     });
 });
--- routes/data.js
+++ routes/data.js
@@ -2,9 +2,16 @@ var loggr = new (require('loggr'))();
 
 exports.get = function(req, res) {
     var metrics = {};
-    for (var i = 0; i < req.query.metrics.length; i++) {
-        var name = req.query.metrics[i];
+
+    for (var i = 0; i < req.query.daily.length; i++) {
+        var name = req.query.daily[i];
         metrics[name] = loggr.getToday({name: name});
     }
+
+    for (var i = 0; i < req.query.ytd.length; i++) {
+        var name = req.query.ytd[i];
+        metrics[name] = loggr.getYTD({name: name});
+    }
+
     res.send(metrics);
 };

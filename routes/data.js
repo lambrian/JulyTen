32a573df1d5ec22c6e26029912f287661d400a0c
Brian Lam <brianlam@stanford.edu>
2014-08-18 21:56:53 -0700

    changed loggr module to support date ranges, extracted day/year logic to router

--- routes/data.js
+++ routes/data.js
@@ -3,14 +3,16 @@ var loggr = new (require('loggr'))();
 exports.get = function(req, res) {
     var metrics = {};
 
+    var currentDay = new Date().setHours(0,0,0,0);
     for (var i = 0; i < req.query.daily.length; i++) {
         var name = req.query.daily[i];
-        metrics[name] = loggr.getToday({name: name});
+        metrics[name] = loggr.getTotalInRange({name: name}, currentDay, new Date());
     }
 
+    var currentYear = new Date(new Date().getFullYear(), 0,1);
     for (var i = 0; i < req.query.ytd.length; i++) {
         var name = req.query.ytd[i];
-        metrics[name] = loggr.getYTD({name: name});
+        metrics[name] = loggr.getTotalInRange({name: name}, currentYear, new Date());
     }
 
     res.send(metrics);
